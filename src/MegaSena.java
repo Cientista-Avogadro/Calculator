0b3f617f13133a85d6246cf38b36dded74936adf
cientista-avogadro <cientistaavogadro2019@gmail.com>
2021-12-24 01:32:38 +0100

    Variavies de escopos

--- src/MegaSena.java
+++ src/MegaSena.java
@@ -3,6 +3,7 @@ public class MegaSena {
   public static void main(String[] args) {
     int x;
     if (true) {
+      int y; //variavel local ou de escopo
       x = 10;
     } else {
       x = 2;
